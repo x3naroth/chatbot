b405e41d9c73bc1861bab2c84260071a66595e9a
x3naroth <xenaroth@gmail.com>
2023-11-16 07:22:22 -0300

    Second version: transformers

--- chatbot.py
+++ chatbot.py
@@ -1,44 +1,60 @@
+import os
 import PyPDF2
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+import torch
+import warnings
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+
+# Suppress transformers and PyTorch warnings
+warnings.filterwarnings("ignore")
+
+# Declare pdf_path as a global variable
+pdf_path = r'route to pdf'
 
 # Load PDF content
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
-        text = ''
+        pdf_text = ""
         for page_num in range(len(pdf_reader.pages)):
-            text += pdf_reader.pages[page_num].extract_text()
-    print("Length of extracted text:", len(text))
-    return text
+            pdf_text += pdf_reader.pages[page_num].extract_text()
+    print("Length of extracted text:", len(pdf_text))
+    return pdf_text
 
-# Process and store document vectors
-def create_document_vectors(text):
-    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-    sentences = [sent.strip() for sent in text.split('.') if sent]
-    vectors = model.encode(sentences)
-    return model, sentences, vectors
+# Answer questions using transformers pipeline
+def answer_question(question, context, model, num_answers=3):
+    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    inputs = tokenizer(question, context, return_tensors="pt")
+    outputs = model(**inputs)
+    answer_start = outputs.start_logits.argmax()
+    answer_end = outputs.end_logits.argmax() + 1
+    answer = tokenizer.decode(inputs["input_ids"][0, answer_start:answer_end])
 
-# Answer questions using document vectors
-def answer_question(question, sentences, vectors):
-    question_vector = model.encode(question)
-    similarities = cosine_similarity([question_vector], vectors)[0]
-    most_similar_idx = similarities.argmax()
-    return sentences[most_similar_idx]
-
+    # Get additional answers
+    n_best_size = min(num_answers, len(outputs.start_logits))
+    answers = []
+    for i in range(n_best_size):
+        start = outputs.start_logits[i].argmax().item()
+        end = outputs.end_logits[i].argmax().item() + 1
+        answers.append(tokenizer.decode(inputs["input_ids"][0, start:end]))
+    
+    return answers
 
 # Main function
 def main():
-    pdf_path = r'pdf path route'
+    global pdf_path  # Declare pdf_path as global
     pdf_text = extract_text_from_pdf(pdf_path)
-    model, sentences, vectors = create_document_vectors(pdf_text)  # Asegúrate de tener 'model' aquí
+
+    # Load the model separately to avoid warnings
+    model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
 
     while True:
         user_question = input("Ask a question (type 'exit' to end): ")
         if user_question.lower() == 'exit':
             break
-        answer = answer_question(user_question, sentences, vectors)
-        print(f"Answer: {answer}")
+        answers = answer_question(user_question, pdf_text, model, num_answers=3)
+        print("Answers:")
+        for i, answer in enumerate(answers):
+            print(f"{i+1}. {answer}")
 
 if __name__ == "__main__":
     main()
